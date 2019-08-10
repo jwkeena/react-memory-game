823f0c07c16881b374f98b45cfd7be483a3f3f1e
jwkeena <jwkeena@gmail.com>
2019-08-10 16:27:58 -0400

    Updates

---
+++ precache-manifest.b9318c22563073557c07ca001f6a3ded.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e6d87c7c09e85fde3e90b35164e9054f",
+    "url": "/jwkeena/react-memory-game/index.html"
+  },
+  {
+    "revision": "c35c371147eff408b417",
+    "url": "/jwkeena/react-memory-game/static/css/main.d1b6784f.chunk.css"
+  },
+  {
+    "revision": "cea528bd8a6c6ccbefab",
+    "url": "/jwkeena/react-memory-game/static/js/2.9918f956.chunk.js"
+  },
+  {
+    "revision": "c35c371147eff408b417",
+    "url": "/jwkeena/react-memory-game/static/js/main.4842d5c1.chunk.js"
+  },
+  {
+    "revision": "0bb8379221fcf6fabd6d",
+    "url": "/jwkeena/react-memory-game/static/js/runtime~main.58b325fd.js"
+  }
+]);
